733d6b4f2dae21d210357d4064728bed4547fb70
cabralchege <cabralowiro@gmail.com>
2020-09-18 17:34:14 +0300

    styling the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {FormControl,Select,MenuItem} from '@material-ui/core';
+import {FormControl,Select,MenuItem,Card,CardContent} from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
 import './App.css';
 
 
@@ -32,27 +34,41 @@ function App() {
   
   return (
     <div className="App">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map(country =>(
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-            
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map(country =>(
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={2000} total={2000}/>
+          <InfoBox title="Recovered" cases={2000} total={2000}/>
+          <InfoBox title="Deaths" cases={2000} total={2000}/>
+        </div>
+
+        
+       {/* Map */}
+       <Map />
       </div>
 
-      <div className="app__stats">
+     <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <h3>Worldwide new cases</h3>
+          {/* Graphs */}
+        </CardContent>
+      </Card>
 
-      </div>
-
- 
-       
     </div>
   );
 }
