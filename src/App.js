ed20196862f27b4214b67ee2bc804a40c454d243
cabralchege <cabralowiro@gmail.com>
2020-10-09 01:19:41 +0300

    added map circle popups

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapZoom, setMapZoom] = useState(3 );
+  const [mapCountries, setMapCountries] = useState([])
 
   useEffect(() =>{
     fetch("https://disease.sh/v3/covid-19/all")
@@ -41,6 +42,7 @@ function App() {
 
         const sortedData = sortData(data);
         setTableData(sortedData);
+        setMapCountries(data);
         setCountries(countries);
     });
   };
@@ -105,6 +107,7 @@ function App() {
 
         
        <Map
+        countries={mapCountries}
         center={mapCenter}
         zoom={mapZoom}
        />
--- src/utilitis.js
+++ src/utilitis.js
@@ -1,3 +1,23 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+    cases: {
+      hex: "#CC1034",
+      multiplier: 800,
+    },
+    recovered: {
+      hex: "#7dd71d",
+      multiplier: 1200,
+    },
+    deaths: {
+      hex: "#fb4443",
+      multiplier: 2000,
+    },
+  };
+
+
 export const sortData = (data) => {
     const sortedData = [...data];
 
@@ -9,4 +29,23 @@ export const sortData = (data) => {
         }
     })
     return sortedData;
-}
+};
+
+export const showDataOnMap = (data, casesType='cases') => (
+    data.map(country => (
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }
+        >
+            <Popup>
+                <h1>IM A POPUP</h1>
+            </Popup>
+        </Circle>
+
+    ))
+);
