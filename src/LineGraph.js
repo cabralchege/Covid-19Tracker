b82bf0cb2490dbfdefb3779df773f45c46bcf865
cabralchege <cabralowiro@gmail.com>
2020-10-10 01:17:03 +0300

    final touches

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -64,7 +64,7 @@ const buildChartData = (data, casesType) => {
     return chartData;
 };
 
-function LineGraph({ casesType }) {
+function LineGraph({ casesType="cases", ...props }) {
     const [data, setData] = useState({});
 
 
@@ -87,7 +87,7 @@ function LineGraph({ casesType }) {
 
 
     return (
-        <div>
+        <div className={props.className}>
             
             {data?.length > 0 && (
                 <Line
